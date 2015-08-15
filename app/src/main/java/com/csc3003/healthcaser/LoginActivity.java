1a8b77400e0d580c043cc4fe8d62fbbce457fc4d
alan_berman <alan.berman.92@gmail.com>
2015-08-15 12:09:51 +0200

    Lot of xml files had duplicate strings. also LoginActivity referenced fields that didn't exist so commented them out

--- app/src/main/java/com/csc3003/healthcaser/LoginActivity.java
+++ app/src/main/java/com/csc3003/healthcaser/LoginActivity.java
@@ -31,80 +31,77 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-<<<<<<< HEAD
         final int duration = Toast.LENGTH_SHORT;
-        loginUsers = new Login();
-        logButton = (Button)findViewById(R.id.login);
-        regButton = (Button)findViewById(R.id.register);
-        editName = (EditText)findViewById(R.id.email);
-        editPass = (EditText)findViewById(R.id.password);
-        //Login
-        logButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                username = editName.getText().toString();
-                password = editPass.getText().toString();
-                if (loginUsers.attemptUserLogin(username, password))
-                {
-                    //Adapted from http://stackoverflow.com/questions/26097513/android-simple-alert-dialog
-                    AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
-                    alertDialog.setTitle("Welcome back, " + username + "!");
-                    alertDialog.setMessage("You've logged in");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                }
-                else
-                {
-                    //Adapted from http://developer.android.com/guide/topics/ui/notifiers/toasts.html
-                    Context context = getApplicationContext();
-                    CharSequence msg = "Login failed.";
-                    Toast errReg = Toast.makeText(context,msg,duration);
-                    errReg.show();
-                }
-
-            }
-        });
-        //Register
-        regButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                username = editName.getText().toString();
-                password = editPass.getText().toString();
-                if (loginUsers.addUser(username, password))
-                {
-
-                    //Adapted from http://stackoverflow.com/questions/26097513/android-simple-alert-dialog
-                    AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
-                    alertDialog.setTitle("Welcome, " + username + "!");
-                    alertDialog.setMessage("You've now registered!");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-
-                }
-                else
-                {
-                    Context context = getApplicationContext();
-                    CharSequence msg = "Registration failed.";
-                    Toast errReg = Toast.makeText(context,msg,duration);
-                    errReg.show();
-                }
-            }
-        });
-=======
+//        loginUsers = new Login();
+//        logButton = (Button)findViewById(R.id.login);
+//        regButton = (Button)findViewById(R.id.register);
+//        editName = (EditText)findViewById(R.id.email);
+//        editPass = (EditText)findViewById(R.id.password);
+//        //Login
+//        logButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                username = editName.getText().toString();
+//                password = editPass.getText().toString();
+//                if (loginUsers.attemptUserLogin(username, password))
+//                {
+//                    //Adapted from http://stackoverflow.com/questions/26097513/android-simple-alert-dialog
+//                    AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+//                    alertDialog.setTitle("Welcome back, " + username + "!");
+//                    alertDialog.setMessage("You've logged in");
+//                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+//                            new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    dialog.dismiss();
+//                                }
+//                            });
+//                    alertDialog.show();
+//                }
+//                else
+//                {
+//                    //Adapted from http://developer.android.com/guide/topics/ui/notifiers/toasts.html
+//                    Context context = getApplicationContext();
+//                    CharSequence msg = "Login failed.";
+//                    Toast errReg = Toast.makeText(context,msg,duration);
+//                    errReg.show();
+//                }
+//
+//            }
+//        });
+//        //Register
+//        regButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                username = editName.getText().toString();
+//                password = editPass.getText().toString();
+//                if (loginUsers.addUser(username, password))
+//                {
+//
+//                    //Adapted from http://stackoverflow.com/questions/26097513/android-simple-alert-dialog
+//                    AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+//                    alertDialog.setTitle("Welcome, " + username + "!");
+//                    alertDialog.setMessage("You've now registered!");
+//                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+//                            new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    dialog.dismiss();
+//                                }
+//                            });
+//                    alertDialog.show();
+//
+//                }
+//                else
+//                {
+//                    Context context = getApplicationContext();
+//                    CharSequence msg = "Registration failed.";
+//                    Toast errReg = Toast.makeText(context,msg,duration);
+//                    errReg.show();
+//                }
+//            }
+//        });
         userDB = new UserDBHandler(this, null, null, 1);
->>>>>>> 00fea8cfaa7eeebea87bf738eebcf539d7e308ec
 
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
-        feedback = (TextView) findViewById(R.id.feedback);
+     //   feedback = (TextView) findViewById(R.id.feedback);
     }
 
     private void redirect (){
