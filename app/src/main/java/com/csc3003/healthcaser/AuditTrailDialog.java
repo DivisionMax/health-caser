c1721020764283bed7b6a6a35516bbbe19b68a53
Gavin Wiener <gavinwiener@gmail.com>
2015-09-13 18:35:28 +0200

    Basic audit trail popup with dummy text

---
+++ app/src/main/java/com/csc3003/healthcaser/AuditTrailDialog.java
@@ -0,0 +1,56 @@
+package com.csc3003.healthcaser;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by GavinW on 2015-09-06.
+ */
+public class AuditTrailDialog extends DialogFragment {
+
+    public static AuditTrailDialog newInstance(String[] input) {
+        AuditTrailDialog frag = new AuditTrailDialog();
+        Bundle args = new Bundle();
+        args.putStringArray("trail", input);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View content = inflater.inflate(R.layout.dialog_audit_trail, null);
+        TextView auditView = (TextView)content.findViewById(R.id.audit_view);
+        //dummy text
+        auditView.setText("Bacon ipsum dolor amet ullamco pork loin doner, tri-tip excepteur velit short loin. Boudin in swine meatball, spare ribs consequat incididunt commodo shankle ham hock cupim voluptate in bresaola filet mignon. Labore short ribs nisi proident andouille adipisicing elit in mollit ex meatloaf doner. Shank adipisicing frankfurter tempor turducken. Commodo cupim consequat biltong kevin minim veniam id fatback meatloaf. Enim occaecat hamburger mollit doner short loin fugiat irure laboris ipsum dolore.\n" +
+                "\n" +
+                "Aliquip hamburger anim pork frankfurter velit, aute ham cillum prosciutto eiusmod landjaeger jerky cupim. Tail minim ribeye nulla meatball bresaola ut ham hock dolor. Jerky chicken est voluptate strip steak meatball incididunt deserunt bresaola. Doner laboris ex short ribs laborum.");
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setView(content)
+                .setTitle(R.string.audit_trail)
+                .setNegativeButton(R.string.test_popup_close, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dismiss();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
--- app/src/main/java/com/csc3003/healthcaser/HealthCaseTestActivity.java
+++ app/src/main/java/com/csc3003/healthcaser/HealthCaseTestActivity.java
@@ -142,6 +142,31 @@ public class HealthCaseTestActivity extends ActionBarActivity implements Diagnos
         information.setText(hc.getStart());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.menu_health_case_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.audit_trail) {
+            //the audit trail must be passed
+            DialogFragment newFragment = AuditTrailDialog.newInstance(new String[1]);
+            newFragment.show(getFragmentManager(), "dialog");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     /*Adapted http://developer.android.com/training/basics/data-storage/files.html*/
     /* Checks if external storage is available for read and write */
     /*public boolean isExternalStorageWritable() {
