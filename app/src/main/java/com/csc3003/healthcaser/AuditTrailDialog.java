1c916a4ce5c12deb8695079f47531d6f3b396e16
Gavin Wiener <gavinwiener@gmail.com>
2015-09-16 16:43:16 +0200

    Changed information view to scrollable, better for smaller phones. audittrail appends info but its bulky

--- app/src/main/java/com/csc3003/healthcaser/AuditTrailDialog.java
+++ app/src/main/java/com/csc3003/healthcaser/AuditTrailDialog.java
@@ -17,30 +17,36 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * Created by GavinW on 2015-09-06.
  */
 public class AuditTrailDialog extends DialogFragment {
 
-    public static AuditTrailDialog newInstance(String[] input) {
+    public static AuditTrailDialog newInstance(ArrayList<String> input) {
         AuditTrailDialog frag = new AuditTrailDialog();
         Bundle args = new Bundle();
-        args.putStringArray("trail", input);
+        args.putStringArrayList("trail", input);
         frag.setArguments(args);
         return frag;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
+        ArrayList<String> auditTrail = getArguments().getStringArrayList("trail");
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View content = inflater.inflate(R.layout.dialog_audit_trail, null);
         TextView auditView = (TextView)content.findViewById(R.id.audit_view);
         //dummy text
-        auditView.setText("Bacon ipsum dolor amet ullamco pork loin doner, tri-tip excepteur velit short loin. Boudin in swine meatball, spare ribs consequat incididunt commodo shankle ham hock cupim voluptate in bresaola filet mignon. Labore short ribs nisi proident andouille adipisicing elit in mollit ex meatloaf doner. Shank adipisicing frankfurter tempor turducken. Commodo cupim consequat biltong kevin minim veniam id fatback meatloaf. Enim occaecat hamburger mollit doner short loin fugiat irure laboris ipsum dolore.\n" +
+        if (auditTrail!=null){
+        for (String s: auditTrail){
+            auditView.setText(s);
+        }
+        }
+/*        auditView.setText("Bacon ipsum dolor amet ullamco pork loin doner, tri-tip excepteur velit short loin. Boudin in swine meatball, spare ribs consequat incididunt commodo shankle ham hock cupim voluptate in bresaola filet mignon. Labore short ribs nisi proident andouille adipisicing elit in mollit ex meatloaf doner. Shank adipisicing frankfurter tempor turducken. Commodo cupim consequat biltong kevin minim veniam id fatback meatloaf. Enim occaecat hamburger mollit doner short loin fugiat irure laboris ipsum dolore.\n" +
                 "\n" +
-                "Aliquip hamburger anim pork frankfurter velit, aute ham cillum prosciutto eiusmod landjaeger jerky cupim. Tail minim ribeye nulla meatball bresaola ut ham hock dolor. Jerky chicken est voluptate strip steak meatball incididunt deserunt bresaola. Doner laboris ex short ribs laborum.");
+                "Aliquip hamburger anim pork frankfurter velit, aute ham cillum prosciutto eiusmod landjaeger jerky cupim. Tail minim ribeye nulla meatball bresaola ut ham hock dolor. Jerky chicken est voluptate strip steak meatball incididunt deserunt bresaola. Doner laboris ex short ribs laborum.")*/;
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         builder.setView(content)
